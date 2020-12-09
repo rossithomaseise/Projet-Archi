8d6695627f53c5549a786c5509110f2f306da620
Thomasfirst <thomasroussarie2000@gmail.com>
2020-12-09 18:29:50 +0100

    becauseImGood

--- projetArchi.c
+++ projetArchi.c
@@ -8,10 +8,12 @@
     int NbCaracLigne = 100;
     int NbLignesPage = 20;
     int caractereActuel = 0;
+    int lastCaracter = 0;
 
     int NbLignes = 0;
     int NbPages = 0;
     int NbPhrases = 0;
+    int NbMots = 0;
 
     int TempLigne = 0;
     int TempPage = 0;
@@ -43,13 +45,31 @@
             {
                 NbPhrases+=1;
             }
+
+            if ((caractereActuel=='.')||(caractereActuel=='!')||(caractereActuel=='?')||(caractereActuel==' ')||(caractereActuel=='\n'))     
+            {
+                if ((caractereActuel=='\n'))
+                {
+                    if ((lastCaracter!='?')||(lastCaracter!='!'))
+                    {
+                        NbMots+=1;
+                    }
+                }
+                else
+                {
+                    NbMots+=1;
+                }
+            }
+
+            lastCaracter=caractereActuel;
+
         } // On continue tant que fgetc n'a pas retourné EOF (fin de fichier)
 
  		NbLignes+=1;	//Pour compter la derniere ligne
 
         fclose(fichier);
 
-        printf("Le fichier à %d lignes.\nEt il à %d pages.\nEt il y à %d phrases\n", NbLignes, NbPages,NbPhrases);
+        printf("Le fichier a %d lignes.\nEt il a %d pages.\nEt il a %d phrases\nEt il a %d mots.\n", NbLignes, NbPages, NbPhrases,NbMots);
     }
  
     return 0;
